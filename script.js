a6489c434a146e1daf1da4128cc6968826f081c1
Baurzhan <baurzhan@Baurzhans-MacBook-Pro.local>
2022-07-13 14:34:28 +0600

    lasts

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const genderBtn = document.getElementById('genderBtn');
+const genderPicture = document.getElementById('genderPicture');
+const shildehana = document.getElementById('shildehana');
+const qyrqynansh = document.getElementById('qyrqynansh');
+const sundettoiTitle = document.getElementById('sundettoiTitle');
+const sundettoiP = document.getElementById('sundettoiP');
+const atqaMingyzu = document.getElementById('atqaMingyzu');
+const myLine = document.getElementById('line');
 
 const screenBtn = document.getElementById('screenButton');
 let isFullScreen = false;
@@ -89,3 +97,34 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+
+let isGirl = false;
+  genderBtn.onclick = ()=>{
+    console.log(isGirl);
+   if(isGirl){
+    genderBtn.innerHTML = `<img src="/img/buttonUl.png" alt="">`;
+    genderPicture.src = "/img/ul.png";
+    isGirl = false;
+    myLine.style.top ="-200px";
+    atqaMingyzu.style.display="unset";
+    shildehana.innerHTML = "Сүйінші хабарды жеткізгендер ұл туса – “ат ұстар” немесе “жылқышы” деп хабарлаған.";
+    qyrqynansh.innerHTML = "Әдетте, ұл баланы 37- 39 күнде қырқынан шығарады";
+    sundettoiTitle.innerHTML = "Sündet toi";
+    sundettoiP.innerHTML = `Қазақта шілдеханадан соң ең атаулы той – сүндет тойы. Ес тоқтатата бастаған баланың өз өміріндегі ағайын-туыс ортасында өзіне деген ықыласты сезініп, құттықтаулар мен бата-тілекті қабылдайтын алғашқы тойы. 
+    </br>
+       Сүндетке отырған баланың да мінез-құлқы бірден өзгереді. Шапқылап ойнап жүретін баланың мінезі салмақтанып, жүріс-тұрысы жинақталып, байсалды қалыпқа түседі. Айналасына да басқаша қарай бастайды.`;
+   }
+   else{
+    genderBtn.innerHTML = `<img src="/img/buttonQyz.png" alt="">`;
+    genderPicture.src = "/img/qyz.png";
+    isGirl = true;
+    myLine.style.top ="-200px";
+    atqaMingyzu.style.display="none";
+    shildehana.innerHTML = "Сүйінші хабарды жеткізгендер қыз туса – “көйлек тігер”, “қырық жеті” немесе “сауыншы туды” деп хабарлаған.";
+    qyrqynansh.innerHTML = "Кей адам қыздың қалың малы көп болсын деп қырқынан 40 күннен асырып шығарады";
+   
+    sundettoiTitle.innerHTML = "Syrğa toi";
+    sundettoiP.innerHTML = `Кішкене қыз бала есейіп, құлағын тесіп, сырға салғанда жасалатын той. Халқымыз "Қызымыз сырғалы, қыз санатты болсын" деп құлағын тестіріп сырға салатын болған. Қыздың басына үкілі тақия кигізеді. Осыдан бастап қыздар өзін басқаша сезінген. "Қызға қырық үйден тыю" деген ұғым бар. Ол осы "сырға тағу" рәсімінен кейін басталады`;
+  }
+  }
